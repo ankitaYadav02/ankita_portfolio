955fa79eb6a131ae52ecfafb6a962ba137375f16
ankitaYadav02 <ankitarao01011998@gmail.com>
2024-03-15 23:17:27 +0530

    fix icons svg's and routes issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ankitaYadav02.github.io",
-  base: "/ankita_portfolio",
+  base: "",
   integrations: [
     react(),
     tailwind({
